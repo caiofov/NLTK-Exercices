ddfe238b2f6f856dbdbbed5207722ee588a1fa6f
Caio Oliveira <69114136+caiofov@users.noreply.github.com>
2022-04-12 16:57:35 +0000

    chapter 2 section 1.1

---
+++ chapter2.py
@@ -0,0 +1,26 @@
+import nltk
+from nltk.corpus import gutenberg as gt
+gt.fileids() #sample texts
+
+emma = gt.words("austen-emma.txt") #returns all words from the text
+
+#print(len(emma)) #number of words
+
+emma_text = nltk.Text(emma) #we need to convert to a text object of the library to use it with some functions
+#print(emma_text.concordance("surprize"))
+
+def show_file_information(fileid): #show some informations about a given text
+  num_chars = len(gt.raw(fileid)) #raw -> all characters of the text. Including spaces and special characters.
+  num_words = len(gt.words(fileid))
+  num_sents = len(gt.sents(fileid)) #sents -> divide the text in sentences
+  num_vocab = len(set(w.lower() for w in gt.words(fileid)))
+
+  avg_word_length = round(num_chars/num_words)
+  avg_sentence_length = round(num_words/num_sents)
+  avg_word_freq = round(num_words/num_vocab)
+  print(avg_word_length, avg_sentence_length, avg_word_freq, fileid)
+
+'''
+for fileid in gt.fileids():
+  show_file_information(fileid)
+'''
